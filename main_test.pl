78f97bbd65c770747859222ef6cb22c3820f88f6
Samuel Quintana <sock.sqt@gmail.com>
2014-10-13 01:37:00 -0500

    Changes to subrutine

--- main_test.pl
+++ main_test.pl
@@ -5,16 +5,17 @@ use Data::Dumper;
 
 my $cache = Services::Meta_Cache->instance();
 my $base_key = "key_";
+my $base_value = "value_hduisdcbdsunds_";
 my $map = {
 	'key' => undef,
-	'value' => "value hduisdcbdsunds",
+	'value' => undef,
 };
 my $duration = "20 minute";
 
-#save_cache($cache);
-#read_cache($cache);
+save_cache($cache);
+read_cache($cache);
 
-print(Dumper($cache));
+#print(Dumper($cache));
 
 sub save_cache {
 	my ($cache) = @_;
@@ -22,7 +23,8 @@ sub save_cache {
 
 	for (my $var = 0; $var < 100; $var++) {
 		$tmp_key = $base_key . $var;
-		$map->{key} = tmp_key;
+		$map->{key} = $tmp_key;
+		$map->{value} = $base_value . $var;
 
 		if ($cache->set($tmp_key, $map, $duration)) {
 			print("Saved key : $tmp_key\n");
@@ -40,7 +42,7 @@ sub read_cache {
 		my $value_map = $cache->get($tmp_key);
 
 		if ($value_map) {
-			print("Readed key $tmp_key = " . Dumper($value_map) . "\n");
+			print("Loaded key $tmp_key = " . Dumper($value_map) . "\n");
 		} else {
 			print("Value for key $tmp_key not found.....\n");
 		}
